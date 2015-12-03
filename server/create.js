56c49b366dce98b80afb05bfe1c7a70530337c9b
Objelisks <supertyrian@gmail.com>
2015-12-02 16:59:39 -0800

    readme, update create script

--- server/create.js
+++ server/create.js
@@ -5,6 +5,7 @@ var db = new sqlite3.Database('./db');
 db.serialize(function() {
   db.run('CREATE TABLE day (date DATE, tagid INTEGER, val INTEGER, PRIMARY KEY (date, tagid))');
   db.run('CREATE TABLE tag (tagid NUMBER, tag NVARCHAR(256), PRIMARY KEY (tagid))');
+  db.run('INSERT INTO tag (tagid, tag) VALUES (0, "mood"), (1, "work"), (2, "play")');
 });
 
 /*
